bf634210b9dba9ad3891d363d2d4259d77117727
cchalc <cchalc>
2021-09-17 03:45:53 +0000

    removed a cell

--- 00b_lakehouse_etl.py
+++ 00b_lakehouse_etl.py
@@ -1,26 +1,4 @@
 # Databricks notebook source
-# MAGIC %md ---
-# MAGIC title: End-to-End MLOps demo with MLFlow, Feature Store and Auto ML, part 0 - Setup
-# MAGIC authors:
-# MAGIC - Rafi Kurlansik
-# MAGIC tags:
-# MAGIC - python
-# MAGIC - telco
-# MAGIC - csv
-# MAGIC - delta
-# MAGIC created_at: 2021-05-01
-# MAGIC updated_at: 2021-05-01
-# MAGIC tldr: End-to-end demo of Databricks for MLOps, including MLflow, the registry, webhooks, scoring, feature store and auto ML. Part 0 - setup of data and Delta table
-# MAGIC ---
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC # Notebook Links
-# MAGIC - AWS demo.cloud: [https://demo.cloud.databricks.com/#notebook/10166850](https://demo.cloud.databricks.com/#notebook/10166850)
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ### Setup
 # MAGIC 
