81236768c10e1f2da6460e3c12e2e35d5d1335c3
cchalc <cchalc>
2021-09-17 04:38:35 +0000

    rename and includes

---
+++ 00_includes.py
@@ -0,0 +1,52 @@
+# Databricks notebook source
+# Load libraries
+import shutil
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from pyspark.sql.functions import col, when
+from pyspark.sql.types import StructType,StructField,DoubleType, StringType, IntegerType, FloatType
+
+# Set config for database name, file paths, and table names
+database_name = 'cchalc_e2eml'
+
+# Move file from driver to DBFS
+user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
+driver_to_dbfs_path = 'dbfs:/home/{}/ibm-telco-churn/Telco-Customer-Churn.csv'.format(user)
+# dbutils.fs.cp('file:/databricks/driver/Telco-Customer-Churn.csv', driver_to_dbfs_path)
+
+# Paths for various Delta tables
+bronze_tbl_path = '/home/{}/ibm-telco-churn/bronze/'.format(user)
+silver_tbl_path = '/home/{}/ibm-telco-churn/silver/'.format(user)
+automl_tbl_path = '/home/{}/ibm-telco-churn/automl-silver/'.format(user)
+telco_preds_path = '/home/{}/ibm-telco-churn/preds/'.format(user)
+
+bronze_tbl_name = 'bronze_customers'
+silver_tbl_name = 'silver_customers'
+automl_tbl_name = 'gold_customers'
+telco_preds_tbl_name = 'telco_preds'
+
+# COMMAND ----------
+
+# Delete the old database and tables if needed
+# uncomment if you need a reset
+# _ = spark.sql('DROP DATABASE IF EXISTS {} CASCADE'.format(database_name))
+
+
+# Create database to house tables
+# _ = spark.sql('CREATE DATABASE {}'.format(database_name))
+# Drop any old delta lake files if needed (e.g. re-running this notebook with the same bronze_tbl_path and silver_tbl_path)
+shutil.rmtree('/dbfs'+bronze_tbl_path, ignore_errors=True)
+shutil.rmtree('/dbfs'+silver_tbl_path, ignore_errors=True)
+shutil.rmtree('/dbfs'+telco_preds_path, ignore_errors=True)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE DATABASE IF NOT EXISTS cchalc_e2eml
+# MAGIC     COMMENT "CREATE A DATABASE WITH A LOCATION PATH"
+# MAGIC     LOCATION "/Users/christopher.chalcraft@databricks.com/databases/cchalc_e2eml" --this must be a location on dbfs (i.e. not direct access)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC USE cchalc_e2eml
--- 00b_lakehouse_etl.py
+++ 00b_lakehouse_etl.py
@@ -21,53 +21,12 @@
 
 # COMMAND ----------
 
-# Load libraries
-import shutil
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from pyspark.sql.functions import col, when
-from pyspark.sql.types import StructType,StructField,DoubleType, StringType, IntegerType, FloatType
+# MAGIC %run ./00_includes
 
-# Set config for database name, file paths, and table names
-database_name = 'cchalc_e2eml'
+# COMMAND ----------
 
-# Move file from driver to DBFS
-user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
-driver_to_dbfs_path = 'dbfs:/home/{}/ibm-telco-churn/Telco-Customer-Churn.csv'.format(user)
+# copy the data from driver to DBFS
 dbutils.fs.cp('file:/databricks/driver/Telco-Customer-Churn.csv', driver_to_dbfs_path)
-
-# Paths for various Delta tables
-bronze_tbl_path = '/home/{}/ibm-telco-churn/bronze/'.format(user)
-silver_tbl_path = '/home/{}/ibm-telco-churn/silver/'.format(user)
-automl_tbl_path = '/home/{}/ibm-telco-churn/automl-silver/'.format(user)
-telco_preds_path = '/home/{}/ibm-telco-churn/preds/'.format(user)
-
-bronze_tbl_name = 'bronze_customers'
-silver_tbl_name = 'silver_customers'
-automl_tbl_name = 'gold_customers'
-telco_preds_tbl_name = 'telco_preds'
-
-# Delete the old database and tables if needed
-_ = spark.sql('DROP DATABASE IF EXISTS {} CASCADE'.format(database_name))
-
-# Create database to house tables
-# _ = spark.sql('CREATE DATABASE {}'.format(database_name))
-# Drop any old delta lake files if needed (e.g. re-running this notebook with the same bronze_tbl_path and silver_tbl_path)
-shutil.rmtree('/dbfs'+bronze_tbl_path, ignore_errors=True)
-shutil.rmtree('/dbfs'+silver_tbl_path, ignore_errors=True)
-shutil.rmtree('/dbfs'+telco_preds_path, ignore_errors=True)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC CREATE DATABASE IF NOT EXISTS cchalc_e2eml
-# MAGIC     COMMENT "CREATE A DATABASE WITH A LOCATION PATH"
-# MAGIC     LOCATION "/Users/christopher.chalcraft@databricks.com/databases/cchalc_e2eml" --this must be a location on dbfs (i.e. not direct access)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC USE cchalc_e2eml
 
 # COMMAND ----------
 
